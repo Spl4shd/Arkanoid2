4a9282aebf1f8a988a9114d782babd28b2ebca8e
Musser <1icmusser@seattleschools.org>
2022-09-19 13:30:34 -0700

    First commit

---
+++ Assets/scripts/Racket.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    public float speed = 150;
+    
+    void FixedUpdate()
+    {
+        //get horizontal input
+        float h = Input.GetaxisRaw("Horizontal");
+        //set velocity (movement direction * Speed)
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
