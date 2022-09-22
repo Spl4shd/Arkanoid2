a20407bb020d30763af0ebd951a4a2ea8a8d687d
Spl4shd <ianmusser29@gmail.com>
2022-09-22 13:13:17 -0700

    Finished Game

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float speed = 100.0f;
+    float hitFactor(Vector2 ballPos,Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x / racketWidth);
+    }
+
+    
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;   
     }  
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //hit the racket?
+        if (col.gameObject.name == "racket")
+        {
+            //calculate hit factor
+            float x=hitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+            
+            //calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            //set velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
 }
--- Assets/scripts/Racket.cs
+++ Assets/scripts/Racket.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Racket : MonoBehaviour
 {
-    public float speed = 150;
-    
+    public float speed = 150;  
+
     void FixedUpdate()
     {
         //get horizontal input
-        float h = Input.GetaxisRaw("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
         //set velocity (movement direction * Speed)
         GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
     }
